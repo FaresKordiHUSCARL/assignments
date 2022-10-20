3ed967ade057b1c6f83217165a9efe77c24a9421
fareskordiHUSCARL/assignment <fareskordiHUSCARL@gmail.com>
2022-10-21 01:34:10 +0300

    new solved challenges

---
+++ HackerRank/InputAndOutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int input, result;
+    cin>> input>> result; result+=input;
+    cin>> input;
+    cout<< result+input;
+    return 0;
+}
