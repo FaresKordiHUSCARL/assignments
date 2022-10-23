4615bd63c0c5ef43eed3f44213ecc52d478ad961
fareskordiHUSCARL/assignment <fareskordiHUSCARL@gmail.com>
2022-10-23 22:11:44 +0300

    new solved challenges 2

---
+++ HackerRank/7-Pointer.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function   
+    int x=*a, y=*b;
+    *a = x+y;
+    if(x-y>=0) *b = x-y;
+    else *b = -(x-y);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
